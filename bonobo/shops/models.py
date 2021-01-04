d1b9e6ca01ebbf489bdab409a7d05d8270afdd4a
Szymon Cader <szymon.sc.cader@gmail.com>
2021-01-04 20:12:07 +0100

    Update template

--- bonobo/shops/models.py
+++ bonobo/shops/models.py
@@ -44,6 +44,7 @@ class ShopQuerySet(QuerySet["Shop"]):
             f'WHERE ST_Distance("shops_shop"."location", ST_GeogFromWKB(\'\\x{point.wkb.hex()}\'::bytea)) <= {radius}'
             f'ORDER BY distance'
         )
+        print(q)
         return self.raw(q)
 
 
